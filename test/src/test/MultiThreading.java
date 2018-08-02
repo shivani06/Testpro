29c441de3212ea6d435d58d8be096d087531e028
shivani06 <shivani6595@gmail.com>
2018-08-02 16:32:34 -0500

    created a demo pprogram for MT

---
+++ test/src/test/MultiThreading.java
@@ -0,0 +1,31 @@
+package test;
+//import java.lang.Thread;
+
+//public class MultiThreading {
+	
+class Hi extends Thread {
+		public void run() {
+			for (int i = 0;i<=5;i++) {
+				System.out.println("Hi");
+				try {Thread.sleep(1000);}catch(Exception e){}
+			}
+		}
+	}
+
+class Hello extends Thread {
+		public void run() {
+			for(int i = 0 ;i<=5;i++) {
+				System.out.println("Hello");
+				try {Thread.sleep(10);}catch(Exception e){}	
+		}
+	}
+}
+public class MultiThreading{
+	public static void main(String[] args) {
+		Hi obj1 = new Hi();
+		Hello obj2 = new Hello();
+		obj1.start();
+		obj2.start();
+	}
+}
+		
