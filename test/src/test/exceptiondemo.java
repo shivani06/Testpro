e7953cb20d2085530d3d5b4719d35f8552c36755
shivani06 <shivani6595@gmail.com>
2018-08-05 13:50:40 -0500

    exceptiion commit

---
+++ test/src/test/exceptiondemo.java
@@ -0,0 +1,29 @@
+package test;
+
+class exceptiondemo2 {
+	public void hello() {
+		System.out.println("hello");
+		
+	}
+}
+
+class hi2 extends exceptiondemo2{
+	
+	public void hi22() {
+		try {
+		super.hello();
+		System.out.println(10/0);
+		}
+		catch(ArithmeticException ex)
+		{
+			System.out.println("Exception caught");
+			}
+		}
+	}
+class exceptiondemo{
+	public static void main(String[] args) {
+		hi2 obj1 = new hi2();
+		obj1.hi22();
+		
+	}
+}
